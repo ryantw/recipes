44556614c3b9c79203871996f776e794979174c6
Ryan <ryantwalk@gmail.com>
2017-12-20 17:58:57 -0500

    Use webjars, make some more tests, display some recipe information

--- src/main/java/io/lker/recipes/repositories/RecipeRepository.java
+++ src/main/java/io/lker/recipes/repositories/RecipeRepository.java
@@ -2,7 +2,7 @@ package io.lker.recipes.repositories;
 
 import io.lker.recipes.model.Recipe;
 import org.springframework.data.repository.CrudRepository;
-
+// CrudRepository implemented through Spring Data JPA
 public interface RecipeRepository extends CrudRepository<Recipe, Long>{
 
 }
